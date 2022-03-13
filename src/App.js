1aafceea8f2ead1e307cc9e0e7fd194165cfacd6
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-03-13 23:14:51 +0300

    created new components for creating new expenses but also rearranged the folder stracture

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import Expenses from "./components/Expenses"
+import Expenses from "./components/Expenses/Expenses"
+import NewExpense from "./components/NewExpense/NewExpense"
+
 function App() {
 	const items = [
 		{
@@ -29,7 +31,7 @@ function App() {
 
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense />
 			<Expenses item={items} />
 		</div>
 	)
