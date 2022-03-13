4148360d6ab7bc6878a1392774c8789fcf37a14a
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-03-13 20:02:48 +0300

    added loging of props.title on click

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,14 +4,17 @@ import ExpenseDate from "./ExpenseDate"
 import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
+	let title = props.title
+
 	const clickHandler = () => {
-		console.log("Clicked")
+		console.log(title)
 	}
+
 	return (
 		<div className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
 			<button onClick={clickHandler}>Change Title</button>
