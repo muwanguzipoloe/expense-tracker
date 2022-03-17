5ef29b9f21bf175eb9f82d7666782b15b900e115
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-03-18 00:37:18 +0300

    Added two way binding to help clear the form on submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,18 +26,26 @@ const ExpenseForm = () => {
 			date: new Date(enteredDate)
 		}
 		console.log(expenseData)
+		setEnteredTitle("")
+		setEnteredAmount("")
+		setEnteredDate("")
 	}
 	return (
 		<form onSubmit={submitHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
 					<input
 						type="number"
+						value={enteredAmount}
 						onChange={amountChangeHandler}
 						min="0.01"
 						step="0.01"
@@ -47,6 +55,7 @@ const ExpenseForm = () => {
 					<label>Date</label>
 					<input
 						type="date"
+						value={enteredDate}
 						onChange={dateChangeHandler}
 						min="2019-01-01"
 						max="2023-12-31"
