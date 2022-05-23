81ac7a96726e9a9db8452210215e00e4e796b4d1
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-05-23 22:31:17 +0300

    Expense item list error corrected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,13 +16,34 @@ function Expenses(props) {
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{props.items.map((expense) => (
+			{props.item.map((expense) => (
 				<ExpenseItem
-					titel={expense.title}
+					title={expense.title}
 					amount={expense.amount}
 					date={expense.date}
 				/>
 			))}
+
+			{/* <ExpenseItem
+				title={props.item[0].title}
+				amount={props.item[0].amount}
+				date={props.item[0].date}
+			/>
+			<ExpenseItem
+				title={props.item[1].title}
+				amount={props.item[1].amount}
+				date={props.item[1].date}
+			/>
+			<ExpenseItem
+				title={props.item[2].title}
+				amount={props.item[2].amount}
+				date={props.item[2].date}
+			/>
+			<ExpenseItem
+				title={props.item[3].title}
+				amount={props.item[3].amount}
+				date={props.item[3].date}
+			/> */}
 		</div>
 	)
 }
