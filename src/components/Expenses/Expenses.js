27d775695c229eb70cbfd9777b68162d9a87f245
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-05-25 01:24:07 +0300

    Added chart graph with calculations to get the graphs working

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css"
 import Card from "./Card"
 import ExpensesFilter from "./ExpensesFilter"
 import ExpensesList from "./ExpensesList"
+import ExpensesChart from "./ExpensesChart"
 
 function Expenses(props) {
 	const [filteredYear, setFilteredYear] = useState("2021")
@@ -21,6 +22,7 @@ function Expenses(props) {
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
+			<ExpensesChart expenses={filteredExpenses} />
 			<ExpensesList items={filteredExpenses} />
 		</div>
 	)
